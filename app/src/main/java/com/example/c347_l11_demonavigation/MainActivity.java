83618a2783779900e910afe539e1012e6c21216f
Bryan Poh <zandrex00@gmail.com>
2020-08-07 20:49:28 +0800

    Done

--- app/src/main/java/com/example/c347_l11_demonavigation/MainActivity.java
+++ app/src/main/java/com/example/c347_l11_demonavigation/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.c347_l11_demonavigation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnNewActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNewActivity = findViewById(R.id.btnNewActivity);
+
+        ((View) btnNewActivity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NewActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
